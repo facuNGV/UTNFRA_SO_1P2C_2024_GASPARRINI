3df23bc80fb4096ea4b0f4bdacc76cd87065e0df
facuNGV <facu_nicogas@outlook.com>
2024-10-08 02:36:37 +0000

    Error nombre directorio en Punto_A

--- RTA_SCRIPT_Examen_20241007/Punto_A.sh
+++ RTA_SCRIPT_Examen_20241007/Punto_A.sh
@@ -2,7 +2,7 @@
 echo "---------------------------------------------------------------------------------------------------------"
 echo "Añadiendo directorios de Examenes-UTN"
 echo "---------------------------------------------------------------------------------------------------------"
-sudo mkdir -p /Examenes-UTN/{alumno_1/parcial_{1..3},alumno_2/parcial_{1..3},alumno_3/parcial{1..3},profesores}
+sudo mkdir -p /Examenes-UTN/{alumno_1/parcial_{1..3},alumno_2/parcial_{1..3},alumno_3/parcial_{1..3},profesores}
 echo "Directorios añadidos: "
 echo " "
 tree /Examenes-UTN
